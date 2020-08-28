d474f986e93190f81d415b0928e59eb0f3dc231e
rhertle <hertle@puzzle.ch>
2020-08-28 13:01:58 +0200

    Remove tests

--- data-transformer/src/test/java/ch/puzzle/NativeDataTransformResourceIT.java
+++
@@ -1,9 +0,0 @@
-package ch.puzzle;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeDataTransformResourceIT extends DataTransformResourceTest {
-
-    // Execute the same tests but in native mode.
-}
