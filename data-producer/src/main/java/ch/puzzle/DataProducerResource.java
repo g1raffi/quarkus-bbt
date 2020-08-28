b7f2cd5051ee5b3a35f7a8cedc91cae279cbe3e5
rhertle <hertle@puzzle.ch>
2020-08-28 11:20:03 +0200

    Add data producer

---
+++ data-producer/src/main/java/ch/puzzle/DataProducerResource.java
@@ -0,0 +1,30 @@
+package ch.puzzle;
+
+import ch.puzzle.entity.BbtDataObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
+
+@Path("/data")
+public class DataProducerResource {
+
+    private final Logger logger = Logger.getLogger(DataProducerResource.class.getName());
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public BbtDataObject getDataObject() {
+        BbtDataObject object = new BbtDataObject();
+        object.value = Math.random();
+        return object;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postDataObject(BbtDataObject object) {
+        logger.info(object.value.toString());
+        return Response.ok(object).build();
+    }
+}
